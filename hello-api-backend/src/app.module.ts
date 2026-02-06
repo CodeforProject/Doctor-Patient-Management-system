376cf2900791941338a114193108841dddd6ba98
abhishek-anuprerna <ksabhishek373@gmail.com>
2026-02-06 11:31:35 +0530

    Data base connecction with;

--- hello-api-backend/src/app.module.ts
+++ hello-api-backend/src/app.module.ts
@@ -1,10 +1,39 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { ConfigModule } from '@nestjs/config';
+
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+
+// export class AppModule {}
+
+
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DbTestService } from './db-test.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: false,  
+      synchronize: false,       
+    }),
+  ],
+  providers: [DbTestService],   // ✅ register DbTestService
 })
 export class AppModule {}
+
